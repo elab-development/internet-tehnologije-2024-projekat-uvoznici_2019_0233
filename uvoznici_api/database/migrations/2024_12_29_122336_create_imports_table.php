4bf32630e9b55f3ef08ebd4040e9b6d73ce1e827
Andrej Pavlović <andrej11.09.00@gmail.com>
2024-12-29 13:40:56 +0100

    Migracije

---
+++ uvoznici_api/database/migrations/2024_12_29_122336_create_imports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('imports', function (Blueprint $table) {
+            $table->id();
+            $table->date('import_date');
+            $table->foreignId('user_id')->constrained();
+            $table->decimal('total_value');
+            $table->string('status')->default('pending');
+            $table->string('reference');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('imports');
+    }
+};
