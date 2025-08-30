29c29cfcc6603374db5a925ef16677b67d44070f
Andrej Pavlović <andrej11.09.00@gmail.com>
2025-08-30 13:40:16 +0200

    Pretraga suppliera

--- uvoznici_api/app/Http/Controllers/SupplierController.php
+++ uvoznici_api/app/Http/Controllers/SupplierController.php
@@ -13,4 +13,17 @@ class SupplierController extends ResponseController
         $suppliers = Supplier::all();
         return $this->successResponse(SupplierResource::collection($suppliers), 'Suppliers fetched successfully');
     }
+
+
+    public function search(Request $request)
+    {
+        $searchText = $request->input('query');
+
+        $suppliers = Supplier::where('name', 'LIKE', "%{$searchText}%")
+            ->orWhere('address', 'LIKE', "%{$searchText}%")
+            ->orWhere('contact_person', 'LIKE', "%{$searchText}%")
+            ->get();
+
+        return $this->successResponse(SupplierResource::collection($suppliers), 'Suppliers fetched successfully');
+    }
 }
